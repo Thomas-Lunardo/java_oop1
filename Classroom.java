3716dbe69590ff651a950f060224ef095e8279bc
THOMAS <thomaslunardo@gmail.com>
2024-12-17 16:12:00 +0100

    push the challenge

---
+++ Classroom.java
@@ -0,0 +1,11 @@
+public class Classroom {
+
+    public static void main(String[] args) {
+
+        Wilder jeanClaude = new Wilder("Jean-Claude"); 
+        System.out.println(jeanClaude.whoAmI());
+        
+        Wilder henri = new Wilder("Henri");
+        System.out.println(henri.whoAmI());
+    }
+}
---
+++ Wilder.java
@@ -0,0 +1,47 @@
+public class Wilder {
+
+    // *** attributes *** //
+    private String firstname;
+    private boolean aware;
+
+    // *** constructors *** //
+    public Wilder(String firstname) {
+        this.firstname = firstname;
+        this.aware = false;
+    }
+
+    public Wilder(String name, boolean aware) {
+        this.firstname = name;
+        this.aware = false;
+    }
+
+    // *** instance method *** //
+    public String whoAmI() {
+
+        String nameSentance = "Je m'appelle " + this.getFirstame();
+
+        if (this.getFirstame() != "Jean-Claude") {
+            return nameSentance + " et je ne suis pas aware.";
+        } else {
+            return nameSentance + " et je suis aware, et 1 + 1 ça fait 11, et ça c'est grand.";
+        }
+    }
+    
+    // *** getters *** //
+    public String getFirstame() {
+        return this.firstname;
+    }
+    
+    public boolean isAware() {
+        return this.aware;
+    }
+    
+    // *** setters *** //
+        public void setFirstame(String firstname) {
+        this.firstname = firstname;
+    }
+    
+    public void setAware(boolean aware) {
+        this.aware = aware;
+    }
+}
